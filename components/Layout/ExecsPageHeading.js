b4374ca3385cc2c1bc7a46116d2121f6a9997795
Parsa Langari <parsa.langari@mail.mcgill.ca>
2022-09-08 17:28:36 -0400

    initial commit

---
+++ components/Layout/ExecsPageHeading.js
@@ -0,0 +1,7 @@
+import styles from '../../styles/Components/Layout/ExecsPageHeading.module.css'
+
+export default function ExecsPageHeading() {
+    return (<div className={styles.container}>
+        <h1>Meet our Execs</h1>
+    </div>)
+}
