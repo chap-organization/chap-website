56c46d8d6a3a164aa722fffd27340fa65febda46
Parsa Langari <parsa.langari@mail.mcgill.ca>
2022-09-09 12:11:30 -0400

    Finalizing it

--- components/Layout/PrimarySideBar.js
+++ components/Layout/PrimarySideBar.js
@@ -11,67 +11,38 @@ export default function PrimarySideBar() {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     return (<nav className={styles.SideBar}>
-        {/* <button className="navbar-toggler" type="button">
-            <span className="terminalicon">Terminal Icon</span>
-        </button> */}
         <div className="bar container-fluid">
-            <Button className={styles.TerminalIcon} onClick={handleShow}>
+            <button className={styles.TerminalIcon} onClick={handleShow}>
                 &gt;_
-            </Button>
+            </button>
             <div className={styles.Circles}>Circles</div>
-            {/* <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                <div className="offcanvas-body">
-                    <ul className="navbar-nav justify-content-end pe-3">
-                        <li className="nav-item">
-                            <a className="nav-link">About Us</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link">The Team</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link">Project and Awards</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link">Media Gallery</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link">Sponsor Us</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link">FAQ</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link">Join us</a>
-                        </li>
-
-                    </ul>
-                </div>
-            </div> */}
             <Offcanvas className={styles.offcanvasBar} show={show} onHide={handleClose} placement={'end'}>
-                <Offcanvas.Header closeButton>
-                    <Offcanvas.Title>Terminal Icon</Offcanvas.Title>
+                <Offcanvas.Header>
+                    <button className={styles.TerminalIcon} onClick={handleClose}>
+                        &lt;_
+                    </button>
                 </Offcanvas.Header>
                 <Offcanvas.Body>
                     <ul className="navbar-nav">
-                        <li className="nav-item py-3">
+                        <li className={styles.PageTitle}>
                             <a className={styles.link}>About Us</a>
                         </li>
-                        <li className="nav-item py-3">
+                        <li className={styles.PageTitle}>
                             <a className={styles.link}>The Team</a>
                         </li>
-                        <li className="nav-item py-3">
+                        <li className={styles.PageTitle}>
                             <a className={styles.link}>Project and Awards</a>
                         </li>
-                        <li className="nav-item py-3">
+                        <li className={styles.PageTitle}>
                             <a className={styles.link}>Media Gallery</a>
                         </li>
-                        <li className="nav-item py-3">
+                        <li className={styles.PageTitle}>
                             <a className={styles.link}>Sponsor Us</a>
                         </li>
-                        <li className="nav-item py-3">
+                        <li className={styles.PageTitle}>
                             <a className={styles.link}>FAQ</a>
                         </li>
-                        <li className="nav-item py-3">
+                        <li className={styles.PageTitle}>
                             <a className={styles.link}>Join us</a>
                         </li>
                     </ul>
