4abad77589935400b09bae450e18dc00b2e78178
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2022-09-13 19:55:33 -0400

    merged exec heading

--- components/general/ExecsPageHeading.js
+++ components/general/ExecsPageHeading.js
@@ -2,12 +2,13 @@ import styles from '../../styles/Components/Layout/ExecsPageHeading.module.css';
 
 export default function ExecsPageHeading({params}) {
 
+    let index = 0;
     return (<div className={styles.container}>
         <img src={"/assets/CHAP.png"} width="200px" height="70px" className={styles.img}/>
         {params.map((line) => {
             const firstLetter = line.toString().slice(0,1).toString();
             const restOfString = line.toString().slice(1, line.length).toString();
-            return (<div className={styles.h1}><h1 ><u>{firstLetter}</u>{restOfString}</h1></div>)
+            return (<div key={index} className={styles.h1}><h1 ><u>{firstLetter}</u>{restOfString}</h1></div>)
         })}
     </div>)
 }
