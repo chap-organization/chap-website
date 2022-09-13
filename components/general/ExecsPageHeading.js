ac47e559165769d7e4c070b97b006ae64befc53e
Parsa Langari <parsa.langari@mail.mcgill.ca>
2022-09-13 19:39:12 -0400

    initial commit

--- components/general/ExecsPageHeading.js
+++ components/general/ExecsPageHeading.js
@@ -1,15 +1,13 @@
 import styles from '../../styles/Components/Layout/ExecsPageHeading.module.css';
 
-export default function ExecsPageHeading({options}) {
+export default function ExecsPageHeading({params}) {
 
-    const firstLineStart = options.firstLine.toString().slice(0,1).toString();
-    const firstLineRest = options.firstLine.toString().slice(1, options.firstLine.length).toString();
-
-    const secondLineStart = options.secondLine.toString().slice(0,1).toString();
-    const secondLineEnd = options.secondLine.toString().slice(1, options.secondLine.length).toString();
     return (<div className={styles.container}>
         <img src={"/assets/CHAP.png"} width="200px" height="70px" className={styles.img}/>
-        <div className={styles.h1}><h1 ><u>{firstLineStart}</u>{firstLineRest}</h1></div>
-        <div className={styles.h1}><h1 ><u>{secondLineStart}</u>{secondLineEnd}</h1></div>
+        {params.map((line) => {
+            const firstLetter = line.toString().slice(0,1).toString();
+            const restOfString = line.toString().slice(1, line.length).toString();
+            return (<div className={styles.h1}><h1 ><u>{firstLetter}</u>{restOfString}</h1></div>)
+        })}
     </div>)
 }
