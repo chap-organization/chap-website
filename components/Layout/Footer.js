2e98a73c7ebd7d0c48b5c184774febffd81e7371
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2022-09-15 14:39:57 -0400

    footer, contact and info

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -1,14 +1,21 @@
 import ContactForm from "../general/ContactForm";
+import FooterInfo from "./FooterInfo";
+
+import styles from "../../styles/Components/Layout/Footer.module.css"
 
 export default function Footer() {
     return (
         <section>
-            <div className="row">
-                <div className="col-md-6">
-                    <ContactForm />
+            <div className="row g-3">
+                <div className={`col-lg-6 ${styles.center}`}>
+                    <div className={styles.contact}>
+                        <ContactForm />
+                    </div>
                 </div>
-                <div className="col-md-6">
-
+                <div className={`col-lg-6 ${styles.center}`}>
+                    <div className={styles.info}>
+                        <FooterInfo />
+                    </div>
                 </div>
             </div>
         </section>
--- components/general/ContactForm.js
+++ components/general/ContactForm.js
@@ -1,36 +1,63 @@
 import styles from "../../styles/Components/General/ContactForm.module.css"
 
 export default function ContactForm() {
+
+    function handleSubmit() {
+        
+        const type = document.querySelector(`.${styles.form} select`).value;
+        const [ firstName, lastName, email, phone ] = document.querySelectorAll(`.${styles.form} input`);
+        const message = document.querySelector(`.${styles.form} textarea`).value;
+
+        // Local input checks
+        // TODO
+
+        const outMessage = {
+            type,
+            firstName: firstName.value,
+            lastName: lastName.value,
+            email: email.value,
+            phone: phone.value,
+            message,
+            
+        }
+        
+        // send email here 
+        // TODO
+    }   
+
     return (
         <div>   
             <div className="row">
                 <p className={styles.joinText}>Shoot us a <br /> message!</p>
             </div>
-            <div className="row">
+            <div className={`row ${styles.formContainer}`}>
                 <form className={styles.form}>
                     <div className="row">
-                        <select className={styles.selectInput} name="ype" >
+                        <select name="type" >
                             <option value="Join Us">Join us</option>
+                            <option value="Sponsor">Sponsor</option>
+                            <option value="GeneralMessage">General Message</option>
                         </select>
                     </div>
-                    <div className="row">
+                    <div className="row g-4 pt-4">
                         <div className="col-6">
-                            <input name="firstName" type="text" placeholder="First Name"></input>
+                            <input name="firstName" id="firstName" type="text" placeholder="First Name"></input>
                         </div>
                         <div className="col-6">
-                            <input name="lastName" type="text" placeholder="Last Name"></input>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-6">
-                            <input name="email" type="text" placeholder="Email"></input>
+                            <input name="lastName" id="lastName" type="text" placeholder="Last Name"></input>
                         </div>
                         <div className="col-6">
-                            <input name="phone" type="number" placeholder="Phone"></input>
+                            <input name="email" id="email" type="text" placeholder="Email"></input>
                         </div>
-                    </div>
-                    <div className="row">
-                        <textarea name="message" placeholder="Why do you want to join?" />
+                        <div className="col-6">
+                            <input name="phone" id="phone" type="tel" placeholder="Phone"></input>
+                        </div>
+                        <div className="col-12">
+                            <textarea name="message" placeholder="Why do you want to join?" />
+                        </div>
+                        <div className={`col-12 ${styles.submitContainer}`} >
+                            <button onClick={handleSubmit} type="button" className={styles.submit}>Submit</button>
+                        </div>
                     </div>
                 </form>
             </div>
