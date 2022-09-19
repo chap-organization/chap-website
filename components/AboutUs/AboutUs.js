d9963764a84e0d1b6ef777c2998e37ee99e79adb
Matthew Cabral <matthew.larbac@gmail.com>
2022-09-19 16:04:15 -0400

    Updated Sidebar Circles Component
    Now the circles are dynamically generated based on the
    section prop passed to them and now changes colors based
    on the appropriate section it is on.

--- components/AboutUs/AboutUs.js
+++ components/AboutUs/AboutUs.js
@@ -1,12 +1,11 @@
-
-import styles from '../../styles/Components/AboutUs/AboutUs.module.css'
-import Chart from './Chart.js'
+import styles from '../../styles/Components/AboutUs/AboutUs.module.css';
+import Chart from './Chart.js';
 
 const AboutUs = () => {
     return (
         <div className={`${styles.container}`}>
             <div className={styles.header}>
-                <h1> ABOUT US </h1>
+                <h1 id="aboutus"> ABOUT US </h1>
             </div>
             <div className={styles.content}>
                 <div className={styles.image}>
@@ -14,17 +13,21 @@ const AboutUs = () => {
                 </div>
                 <div className={`${styles.bottomContent} row`}>
                     <div className={`${styles.bottomText} col-lg-6`}>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the
+                        industry's standard dummy text ever since the 1500s.
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the
+                        industry's standard dummy text ever since the 1500s.
                     </div>
                     <div className={`${styles.chart} col-lg-6`}>
                         <p>Demographics</p>
-                        <Chart/>
+                        <Chart />
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
-
+    );
+};
 
 export default AboutUs;
--- components/general/ContactForm.js
+++ components/general/ContactForm.js
@@ -1,11 +1,14 @@
-import styles from "../../styles/Components/General/ContactForm.module.css"
+import styles from '../../styles/Components/General/ContactForm.module.css';
 
 export default function ContactForm() {
-
     function handleSubmit() {
         const type = document.querySelector(`.${styles.form} select`).value;
-        const [ firstName, lastName, email, phone ] = document.querySelectorAll(`.${styles.form} input`);
-        const message = document.querySelector(`.${styles.form} textarea`).value;
+        const [firstName, lastName, email, phone] = document.querySelectorAll(
+            `.${styles.form} input`
+        );
+        const message = document.querySelector(
+            `.${styles.form} textarea`
+        ).value;
 
         // Local input checks
         // TODO
@@ -17,44 +20,77 @@ export default function ContactForm() {
             email: email.value,
             phone: phone.value,
             message,
-        }
-        
-        // send email here 
+        };
+
+        // send email here
         // TODO
-    }   
+    }
 
     return (
-        <div>   
+        <div>
             <div className="row">
-                <p className={styles.joinText}>Shoot us a <br /> message!</p>
+                <p className={styles.joinText} id="contactus">
+                    Shoot us a <br /> message!
+                </p>
             </div>
             <div className={`row ${styles.formContainer}`}>
                 <form className={styles.form}>
                     <div className="row">
-                        <select name="type" className={"form-select"} >
+                        <select name="type" className={'form-select'}>
                             <option value="Join Us">Join us</option>
                             <option value="Sponsor">Sponsor</option>
-                            <option value="GeneralMessage">General Message</option>
+                            <option value="GeneralMessage">
+                                General Message
+                            </option>
                         </select>
                     </div>
                     <div className="row g-4 pt-4">
                         <div className="col-6">
-                            <input name="firstName" id="firstName" type="text" placeholder="First Name"></input>
+                            <input
+                                name="firstName"
+                                id="firstName"
+                                type="text"
+                                placeholder="First Name"
+                            ></input>
                         </div>
                         <div className="col-6">
-                            <input name="lastName" id="lastName" type="text" placeholder="Last Name"></input>
+                            <input
+                                name="lastName"
+                                id="lastName"
+                                type="text"
+                                placeholder="Last Name"
+                            ></input>
                         </div>
                         <div className="col-6">
-                            <input name="email" id="email" type="email" placeholder="Email"></input>
+                            <input
+                                name="email"
+                                id="email"
+                                type="email"
+                                placeholder="Email"
+                            ></input>
                         </div>
                         <div className="col-6">
-                            <input name="phone" id="phone" type="tel" placeholder="Phone"></input>
+                            <input
+                                name="phone"
+                                id="phone"
+                                type="tel"
+                                placeholder="Phone"
+                            ></input>
                         </div>
                         <div className="col-12">
-                            <textarea name="message" placeholder="Why do you want to join?" />
+                            <textarea
+                                name="message"
+                                placeholder="Why do you want to join?"
+                            />
                         </div>
-                        <div className={`col-12 ${styles.submitContainer}`} >
-                            <button onClick={handleSubmit} type="button" className={styles.submit}>Submit</button>
+                        <div className={`col-12 ${styles.submitContainer}`}>
+                            <button
+                                onClick={handleSubmit}
+                                type="button"
+                                className={styles.submit}
+                            >
+                                Submit
+                            </button>
                         </div>
                     </div>
                 </form>
