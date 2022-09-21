e060f019e37d6ee8b2be6c5dc1d615e9920e419e
smahm01 <sadek.mahmood@mail.mcgill.ca>
2022-09-21 10:14:34 -0400

    CHAPW-29 Created FAQPage component and FAQPage header

---
+++ components/FAQ/FAQHeading.js
@@ -0,0 +1,11 @@
+import styles from "../../styles/Components/FAQ/FAQHeading.module.css"
+
+export default function FAQHeading() {
+    return (
+        <div className={styles.container}>
+            <p style={{marginLeft: "30px"}}>FAQ</p>
+        </div>
+    )
+
+
+}
